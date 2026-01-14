c200e21ca511669a1d6bd78cbd8fa3421da0479d
Kauan Henrique <kauanbrpe.dev@hotmail.com>
2026-01-14 17:16:24 -0300

    fix: projects and hero

--- src/core-components/hero.tsx
+++ src/core-components/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
 };
     
     return (
-        <section className="w-full bg-black-initial flex flex-col md:flex-row items-center justify-center md:h-135 h-auto py-8 md:py-0">
+        <section className="w-full bg-black-initial flex flex-col md:flex-row items-center justify-center md:h-135 h-auto py-8 sm:py-0">
             <div className="px-4 sm:px-6 gap-4 space-y-3 text-center md:text-left">
                 <Text as="h1" variant="body-bold-title">Kauan Henrique</Text>
                 <Text as="h2" variant="body-bold-subtile">Dev Back-end | ADS - UNIT</Text>
@@ -21,7 +21,7 @@ export default function Hero() {
             </div>
 
             <div className="md:ml-45 mt-6 md:mt-0 shrink-0">
-                <img src={imgKauan} alt="imagem Kauan Henrique" className="h-40 w-40 md:h-86 md:w-86 rounded-4xl object-cover" />
+                <img src={imgKauan} alt="imagem Kauan Henrique" className="h-86 w-86 rounded-4xl object-cover" />
             </div>
         </section>
     )
